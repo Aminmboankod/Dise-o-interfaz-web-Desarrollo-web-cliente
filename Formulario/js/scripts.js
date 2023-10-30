887136c28a7569e34754dbbe1664783662a41831
Aminmboankod <amin.m.boankod@gmail.com>
2023-10-30 21:13:55 +0100

    añadir formulario

---
+++ Formulario/js/scripts.js
@@ -0,0 +1,20 @@
+function validateForm() {
+  var name = document.forms["registrationForm"]["name"].value;
+  var surname1 = document.forms["registrationForm"]["surname1"].value;
+  var dni = document.forms["registrationForm"]["dni"].value;
+  var cp = document.forms["registrationForm"]["cp"].value;
+  var maritalStatus = document.forms["registrationForm"]["maritalStatus"].value;
+  var employeeNumber = document.forms["registrationForm"]["employeeNumber"].value;
+  var user = document.forms["registrationForm"]["user"].value;
+  var department = document.forms["registrationForm"]["department"].value;
+  var startDate = document.forms["registrationForm"]["startDate"].value;
+
+  if (name === "" || surname1 === "" || dni === "" || cp === "" || maritalStatus === "" || employeeNumber === "" || user === "" || department === "" || startDate === "") {
+      alert("Todos los campos marcados con * son obligatorios.");
+      return false;
+  }
+
+  // Puedes agregar más validaciones específicas para cada campo según tus requisitos.
+
+  return true;
+}
